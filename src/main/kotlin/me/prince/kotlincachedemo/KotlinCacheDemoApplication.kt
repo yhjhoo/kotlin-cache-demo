fdaf24bcd1a7790d3456530490eb95a1a31e5961
yhjhoo <yhjhoo@163.com>
2020-11-01 11:47:08 +0800

    Simply with code config

--- src/main/kotlin/me/prince/kotlincachedemo/KotlinCacheDemoApplication.kt
+++ src/main/kotlin/me/prince/kotlincachedemo/KotlinCacheDemoApplication.kt
@@ -1,25 +1,34 @@
 package me.prince.kotlincachedemo
 
+import com.github.benmanes.caffeine.cache.Caffeine
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 
 @SpringBootApplication
 @EnableCaching
-class KotlinCacheDemoApplication
+class KotlinCacheDemoApplication {
+    @Bean
+    fun caffeineConfig(): Caffeine<*, *> {
+        return Caffeine.newBuilder().expireAfterWrite(10, TimeUnit.SECONDS)
+    }
+}
 
 
 fun main(args: Array<String>) {
     runApplication<KotlinCacheDemoApplication>(*args)
 }
+
 
 @RestController
 class CacheController {
@@ -36,6 +45,7 @@ class CacheController {
 class CacheService {
     @Cacheable("kotlinCache")
     fun invoke(id: Int): String {
+        println("hello")
         val nextInt = Random().nextInt()
         return "${id}_${nextInt}"
     }
